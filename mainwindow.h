cbc596b828f699ad817408c600648079bd6da365
John West <johnwest@Johns-MacBook-Pro-4.local>
2019-07-04 09:51:10 -0700

    First commit

---
+++ mainwindow.h
@@ -0,0 +1,31 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+
+namespace Ui {
+class MainWindow;
+}
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+private slots:
+    void on_actionOpen_triggered();
+
+    void on_actionPlay_triggered();
+
+    void on_actionPause_triggered();
+
+    void on_actionStop_triggered();
+
+private:
+    Ui::MainWindow *ui;
+};
+
+#endif // MAINWINDOW_H
